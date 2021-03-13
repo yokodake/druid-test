f851796d2acce67a5c9d8427bb89424253d0b2d2
yokodake <yokodake@cinnabar.fr>
2021-03-13 15:06:56 +0100

    with state

--- lib/src/state.rs
+++ lib/src/state.rs
@@ -1,18 +1,55 @@
-use std::path::PathBuf;
-use std::env;
+use druid::{Data, Lens};
 
+use std::env;
+use std::path::{Path, PathBuf};
+use std::{sync::Arc};
+
+/// @FIXME: decouple druid from yukari_lib
+#[derive(Clone, Data, Lens)]
 pub struct State {
-    parent: Option<PathBuf>,
-    current: PathBuf,
+    #[data(same_fn = "PartialEq::eq")]
+    pub parent: Option<PathBuf>,
+    #[data(same_fn = "PartialEq::eq")]
+    pub current: PathBuf,
+    #[data(ignore)]
+    current_content: Arc<Vec<String>>,
+    #[data(ignore)]
+    parent_content: Arc<Vec<String>>,
 }
 
 impl State {
-    fn cwd() -> Self {
-        let current =
-            env::current_dir()
+    /// falls back to home
+    pub fn cwd() -> Self {
+        let current = env::current_dir()
             .or_else(|_| env::home_dir().ok_or(()))
             .unwrap_or("/".into());
-        let parent = None;
-        Self { parent, current }
+        let parent = current.parent().map(|p| p.to_owned());
+        let mut state = Self {
+            current,
+            parent,
+            current_content: Default::default(),
+            parent_content: Default::default(),
+        };
+        state.update();
+        state
     }
+
+    pub fn update(&mut self) {
+        self.current_content = Arc::new(Self::dir_contents(&self.current));
+        self.parent_content = Arc::new(self.parent.as_ref().map_or(vec![], Self::dir_contents));
+    }
+
+    pub fn dir_contents(path: impl AsRef<Path>) -> Vec<String> {
+        match path.as_ref().read_dir().and_then(|it| {
+            it.map(|r| r.map(|de| de.file_name().to_string_lossy().into_owned()))
+                .collect()
+        }) {
+            Ok(cs) => cs,
+            Err(_) => vec![],
+        }
+    }
+}
+
+struct DirInfo {
+    num_children: u32,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,53 @@
-use std::sync::Arc;
-
-use druid::lens::{Constant, InArc, Identity, Then};
-use druid::widget::{Container, Flex, Label, List, Split, LensWrap};
+use druid::widget::{Container, Flex, Label, List, Split};
 use druid::{
-    AppDelegate, AppLauncher, Code, Data, Event, Lens, PlatformError, Size, Widget, WindowDesc
+    AppDelegate, AppLauncher, Code, Data, Event, PlatformError, Size, Widget,
+    WidgetExt, WindowDesc,
 };
 
+use yukari_lib::State;
 use yukari_widgets::Footer;
 
-#[derive(Lens, Clone, Data)]
-struct Test {
-    #[data(same_fn = "PartialEq::eq")]
-    header: String,
-    #[data(same_fn = "PartialEq::eq")]
-    footer: String,
-    #[data(same_fn = "PartialEq::eq")]
-    title: String,
-    #[data(ignore)]
-    child1: Arc<Vec<String>>,
-    #[data(ignore)]
-    child2: Arc<Vec<String>>,
-}
-
-fn build_header(body: impl Widget<Test> +'static) -> impl Widget<Test> {
+// @FIXME fill entire body like footer
+fn build_header<T: Data>(body: impl Widget<T> + 'static) -> impl Widget<T> {
     Flex::column()
-        .with_flex_child(Label::dynamic(|data: &Test, _| data.header.clone()), 2.)
+        .with_flex_child(Label::new("menu.."), 1.)
         .with_flex_child(body, 1.)
         .must_fill_main_axis(true)
 }
-fn build_footer(body: impl Widget<Test> +'static) -> impl Widget<Test> {
+fn build_footer(body: impl Widget<State> + 'static) -> impl Widget<State> {
     Footer::new(
         body,
         Flex::row()
-            .with_flex_child(Label::dynamic(|data :&Test, _| data.footer.clone()), 2.)
+            .with_flex_child(
+                Label::dynamic(|data: &State, _| data.current.to_string_lossy().into_owned()),
+                2.,
+            )
             .must_fill_main_axis(true),
     )
     .fill_body(true)
     .border(druid::Color::RED, 1.)
 }
-fn build_main() -> impl Widget<Test> {
+fn build_main() -> impl Widget<State> {
     Split::columns(
-        LensWrap::new(
-            List::new(|| Label::new("left")),
-            // Then::new(Constant(vec![()]), InArc::new(Identity)),
-            Test::child1
-        ),
-        LensWrap::new(
-            List::new(|| Label::new("right")),
-            Test::child2
-            // Then::new(Test::child2, InArc::new(Identity)),
-            // Then::new(Constant(vec![()]), InArc::new(Identity)),
-        ),
+        List::new(|| Label::dynamic(|d: &String, _| d.clone()))
+            .lens(State::parent_content),
+        List::new(|| Label::dynamic(|d: &String, _| d.clone()))
+            .scroll()
+            .vertical()
+            .lens(State::current_content),
     )
     .draggable(true)
     .solid_bar(true)
     .min_size(150., 300.)
 }
 
-fn build_ui() -> impl Widget<Test> {
+fn build_ui() -> impl Widget<State> {
     Container::new(build_footer(build_main()))
 }
 
 fn main() -> Result<(), PlatformError> {
-    fn update_title(t: &Test, _: &druid::Env) -> String {
-        t.title.clone()
+    fn update_title(data: &State, _: &druid::Env) -> String {
+        data.current.to_string_lossy().into_owned()
     }
     AppLauncher::with_window(
         WindowDesc::new(build_ui())
@@ -71,29 +55,18 @@ fn main() -> Result<(), PlatformError> {
             .with_min_size(Size::new(300., 300.)),
     )
     .delegate(QExist {})
-    .launch(Test {
-        header: "menu..".into(),
-        footer: "footer".into(),
-        title: "one".into(),
-        child1: Arc::new(vec![
-            "first".into(),
-            "second".into(),
-            "third".into(),
-            "fourth".into(),
-        ]),
-        child2: Arc::new(vec!["first".into(), "second".into()]),
-    })
+    .launch(State::cwd())
 }
 
 struct QExist {}
 
-impl AppDelegate<Test> for QExist {
+impl<T: Data> AppDelegate<T> for QExist {
     fn event(
         &mut self,
         _ctx: &mut druid::DelegateCtx,
         _window_id: druid::WindowId,
         event: Event,
-        data: &mut Test,
+        _data: &mut T,
         _env: &druid::Env,
     ) -> Option<Event> {
         match &event {
@@ -101,13 +74,6 @@ impl AppDelegate<Test> for QExist {
                 println!("{}", k.code);
                 if k.code == Code::KeyQ {
                     std::process::exit(1)
-                } else if k.code == Code::KeyW {
-                    data.title = if data.title == "one" {
-                        "two".into()
-                    } else {
-                        "one".into()
-                    };
-                    None
                 } else {
                     Some(event)
                 }
@@ -121,7 +87,7 @@ impl AppDelegate<Test> for QExist {
         _ctx: &mut druid::DelegateCtx,
         _target: druid::Target,
         _cmd: &druid::Command,
-        _data: &mut Test,
+        _data: &mut T,
         _env: &druid::Env,
     ) -> druid::Handled {
         druid::Handled::No
@@ -130,7 +96,7 @@ impl AppDelegate<Test> for QExist {
     fn window_added(
         &mut self,
         _id: druid::WindowId,
-        _data: &mut Test,
+        _data: &mut T,
         _env: &druid::Env,
         _ctx: &mut druid::DelegateCtx,
     ) {
@@ -139,7 +105,7 @@ impl AppDelegate<Test> for QExist {
     fn window_removed(
         &mut self,
         _id: druid::WindowId,
-        _data: &mut Test,
+        _data: &mut T,
         _env: &druid::Env,
         _ctx: &mut druid::DelegateCtx,
     ) {
